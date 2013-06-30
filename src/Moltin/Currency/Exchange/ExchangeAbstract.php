cdaddafc51c6d03c0a4777588395937a662ecede
Chris Harvey <chris@chrisnharvey.com>
2013-06-30 16:50:15 +0000

    Removing get method from Exchange

--- src/Moltin/Currency/Exchange/ExchangeAbstract.php
+++ src/Moltin/Currency/Exchange/ExchangeAbstract.php
@@ -40,9 +40,4 @@ abstract class ExchangeAbstract
             if (isset($this->data[$key])) $this->data[$key] = $value;
         }
     }
-
-    public function get($code)
-    {
-        return $this->store->get($code);
-    }
 }
